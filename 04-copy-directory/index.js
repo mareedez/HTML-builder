6e229536536a8677eb3d075b529464b6a0eadbd8
mareedez <xxl_century@yahoo.com>
2022-05-22 11:57:50 -0700

    feat: copy-directory deep

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,18 +5,24 @@ const path = require('path');
 const copySource = path.join(__dirname, 'files');
 const copyDestination = path.join(__dirname, 'files-copy');
 
-
-async function cloneDir(){
-  await fs.promises.rm(copyDestination, {recursive: true, force: true});
-  await fs.promises.mkdir(copyDestination, {recursive: true});
-  const files = await fs.promises.readdir(copySource);
+async function cloneDir(source, destination){
+  await fs.promises.rm(destination, {recursive: true, force: true});
+  await fs.promises.mkdir(destination, {recursive: true});
+  let files = await fs.promises.readdir(source, {withFileTypes: true});
   files.forEach((file) => {
-    try {
-      fs.promises.copyFile(path.join(copySource, file), path.join(copyDestination, file));
-    } catch {
-      console.log(`${file} could not be copied`);
+    let fileSource = path.join(source, file.name);
+    let fileDestination = path.join(destination, file.name);
+    if(file.isDirectory()){
+      cloneDir(fileSource, fileDestination);
+    } else {
+      try {
+        fs.promises.copyFile(path.join(source, file.name), path.join(destination, file.name));
+      } catch {
+        console.log(`${file} could not be copied`);
+      }
     }
   });
 }
 
-cloneDir();
+cloneDir(copySource, copyDestination);
+
