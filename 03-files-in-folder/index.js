3129ebac55f7496eb81edf233b51ce37fe141061
mareedez <xxl_century@yahoo.com>
2022-05-21 22:15:06 -0700

    feat: files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const fs = require('fs/promises');
+
+const directory = path.join(__dirname, 'secret-folder');
+
+async function readFiles(){
+  try {
+    const files = await fs.readdir(directory, {withFileTypes: true});
+    for(let file of files){
+      const data = await fs.stat(path.join(directory, file.name));
+      if(data.isFile()){
+        const name = file.name.slice(0, file.name.lastIndexOf('.'));
+        const size = `${data.size}b`;
+        const extension = `- ${file.name.slice(file.name.lastIndexOf('.') + 1)} -`;
+        console.log(name, extension, size);
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+readFiles();
