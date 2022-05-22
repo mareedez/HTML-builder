106a49c433fdefc96be973c42e11b4bb268b1370
mareedez <xxl_century@yahoo.com>
2022-05-22 11:14:12 -0700

    feat: copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,22 @@
+const fs = require('fs');
+const path = require('path');
 
+
+const copySource = path.join(__dirname, 'files');
+const copyDestination = path.join(__dirname, 'files-copy');
+
+
+async function cloneDir(){
+  await fs.promises.rm(copyDestination, {recursive: true, force: true});
+  await fs.promises.mkdir(copyDestination, {recursive: true});
+  const files = await fs.promises.readdir(copySource);
+  files.forEach((file) => {
+    try {
+      fs.promises.copyFile(path.join(copySource, file), path.join(copyDestination, file));
+    } catch {
+      console.log(`${file} could not be copied`);
+    }
+  });
+}
+
+cloneDir();
